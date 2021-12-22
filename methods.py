2c79c22ccc1d3733960286a40d14dddd948d363a
tejasg13 <91737398+tejasg13@users.noreply.github.com>
2021-12-22 18:01:28 +0530

    Add files via upload

---
+++ methods.py
@@ -0,0 +1,20 @@
+from datetime import date
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    @classmethod
+    def birthyear(cls,name,year):
+        return cls(name,date.today().year - year)
+
+    @staticmethod
+    def isadult(age):
+        return age>18
+
+p1 = Person("Tejas",18)
+p2 = Person.birthyear("Preetha",2010)
+
+print(p1.age)
+print(p2.age)
+print(Person.isadult(20))
+        
